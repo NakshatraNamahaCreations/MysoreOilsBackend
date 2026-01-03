6ef42b262111f6d7bb150554d72f3bb921527e60
NakshatraNamahaCreations <ops.nnc@gmail.com>
2026-01-03 13:09:45 +0530

    final code

--- Controllers/customerController.js
+++ Controllers/customerController.js
@@ -1,4 +1,4 @@
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const User = require("../models/Customer");
 const crypto = require("crypto");
 const nodemailer = require("nodemailer");
